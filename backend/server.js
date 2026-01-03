363ac659f37ccf3d697202799fa321be73c57d9b
Pratham K P <prathamkpsagar@gmail.com>
2026-01-03 12:29:28 +0530

    Added nodemon and handled some extra routes for understanding

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,15 @@ const port= 5001;
 app.get("/api/notes", (req,res)=>{
     res.send("<h1>You have not created any notes yet</h1>");
 })
+app.post("/api/notes", (req, res) =>{
+    res.status(201).json({message:"Note created successfully"})
+})
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note updated successfully"})
+})  
+app.delete("/api/notes", (req, res) => {
+    res.status(200).json({message:"Note deleted successfully"})
+})  
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
 });
