6b4fd22d4f61114b26bd28493c5e7555546ea724
Pratham K P <prathamkpsagar@gmail.com>
2026-01-06 12:24:04 +0530

    Added rate limiting using upstash middleware

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js"
 
 export async function getAllNotes (req, res){
     try {
-        const notes = await Note.find().sort({createdAt: -1}) //fetches all notes
+        const notes = await Note.find().sort({createdAt: -1}) //fetches all notes -1 will give newest first
         res.status(200).json(notes);
     } catch (error) {
         console.log("Error in getAllNotes controller", error)
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,26 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config()
 const app= express();
 
 const port= process.env.PORT || 5001;
 
-connectDB();
+
 
 app.use(express.json()) //middleware
-
+app.use(rateLimiter)
+app.use( (req, res, next)=>{
+    console.log(`Req method is ${req.method} and request url is ${req.url}`);
+    next();
+});
 app.use("/api/notes", notesRoutes)
 
-app.listen(port, ()=>{
-    console.log("Server listening on port ", port)
-});
-
+connectDB().then( () => {
+    app.listen(port, ()=>{
+        console.log("Server listening on port ", port)
+    });
+})
 // mongodb+srv://prathamkpsagar_db_user:kXArS7zREaCzK8IU@cluster0.uivwbdk.mongodb.net/?appName=Cluster0
