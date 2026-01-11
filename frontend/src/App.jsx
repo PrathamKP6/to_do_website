9cd5f03740df48b5fe0ee4c2c85cc9bc31d818ac
Pratham K P <prathamkpsagar@gmail.com>
2026-01-11 11:35:38 +0530

    Completed all the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
-import { HomePage } from './pages/HomePage'
+import  HomePage  from './pages/HomePage'
 import  CreatePage  from './pages/CreatePage'
-import { NoteDetailPage } from './pages/NoteDetailPage'
+import NoteDetailPage from './pages/NoteDetailPage'
 import toast from "react-hot-toast"
 
 const App = () => {
